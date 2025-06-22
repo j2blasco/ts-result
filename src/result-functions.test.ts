e3a8ddf95d318de9d534c39cf717dfa8dd134f88
blasco <blascoburguillos@gmail.com>
2025-06-22 20:02:48 +0200

    fix: building and test passing

--- src/result-functions.test.ts
+++ src/result-functions.test.ts
@@ -1,53 +1,34 @@
-import { pipe } from "@j2blasco/pipe";
+import { asyncPipe, pipe } from "@j2blasco/ts-pipe";
+import { andThen, andThenAsync } from "./result-functions";
 import { resultSuccess } from "./result";
+import { assertType } from "./utils/test/assert-type";
 
 function doSomething() {
   return resultSuccess("5");
 }
 
-function doSomething2() {
-  return resultSuccess("5");
-}
+describe("Result integration with ts-pipe", () => {
+  test("asyncPipe", async () => {
+    var result = await asyncPipe(
+      doSomething(),
+      andThenAsync(async (_e) => resultSuccess(new Date())),
+      andThen((r) => {
+        assertType<typeof r, Date>(true);
+        return resultSuccess("test");
+      })
+    );
+    expect(result.unwrapOrThrow()).toBe("test");
+  });
 
-var result = doSomething();
-
-var resultPipeSimple = pipe(doSomething(), (test) =>
-  test.andThen((t) => resultSuccess(5))
-);
-
-var resultPipePromise = pipe(
-  doSomething(),
-  (test) => test.andThenAsync(async (e) => resultSuccess(new Date())),
-  async (test) => await test
-);
-
-var resultPipeSimple = pipe(
-  doSomething(),
-  andThen((t) => resultSuccess(5))
-);
-
-var resultPipe = pipe(
-  doSomething(),
-  andThenAsync(async (_e) => resultSuccess(new Date()))
-);
-
-async function test() {
-  var resultPipe = await asyncPipe(
-    doSomething(),
-    andThenAsync(async (_e) => resultSuccess(new Date())),
-    andThen((r) => resultSuccess("hey"))
-  );
-}
-
-async function test2() {
-  var result = await pipe(
-    doSomething(),
-    andThenAsync(async (_e) => resultSuccess(new Date()))
-  );
-
-  var resultAsync = await asyncPipe(
-    result,
-    andThenAsync(async (r) => resultSuccess("hey")),
-    andThen((r) => resultSuccess(r.toUpperCase()))
-  );
-}
+  test("pipe", async () => {
+    var result = pipe(
+      doSomething(),
+      andThen((_e) => resultSuccess(new Date())),
+      andThen((r) => {
+        assertType<typeof r, Date>(true);
+        return resultSuccess("test");
+      })
+    );
+    expect(result.unwrapOrThrow()).toBe("test");
+  });
+});
